74d3c1815c0f435618a527186c1ea2b3501f28d0
Dmytro Havatiuk <d.havatiuk@limesoda.com>
2016-10-31 16:01:25 +0100

    create first programm

---
+++ classArray.java
@@ -0,0 +1,44 @@
+//package com.mkyong;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by dhavatiuk on 31.10.16.
+ */
+public class classArray {
+    public static void main(String [] args) {
+
+
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+                System.out.print("Enter something: ");
+                String input = br.readLine();
+
+                if ("q".equals(input)) {
+                    System.out.println("Exit!");
+                    System.exit(0);
+                }
+
+                System.out.println("input :" + input);
+                System.out.println("----------------\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
